aa1be52d0a7d7d9756671791b637097be55314a8
laurengranada <laurengranada@utexas.edu>
2017-04-17 20:44:46 -0500

    adding Drake style hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,143 @@
-// // window.onload = function(){
 
-		var words = ["hotline bling", "controlla", "one dance", 
-		"practice", "come thru", "worst behavior", "know yourself", 
-		"diamonds dancing", "jumpman", "childs play", "too good", 
-		"pop style", "fake love", "over", "back to back", "summer sixteen", 
-		"successful", "doing it wrong"];
+// array of words
+var words = ["hotline bling", "controlla", "one dance", "energy"];
+// word chosen from array
+var word = "";
+// represents individual letters in word
+var lettersInWord = [];
+// spaces for hidden word
+var spaces = 0;
+// shows letters guessed and spaces remaining
+var lettersAndSpaces = [];
+// letters guessed
+var lettersGuessed = [];
+// audio objects for songs to corresponding word
+var energyAudio = new Audio('assets/audio/energy.mp3');
+var controllaAudio = new Audio('assets/audio/controlla.mp3');
+var hotlineAudio = new Audio('assets/audio/hotline.mp3');
 
-		var wins = 0;
-		var chances = 10;
+
+
+// setting variables for wins, losses
+var wins = 0;
+var losses = 0;
+// chances left
+var chances = 14;
+
+
+function startGame() {
+	// stop track playing
+	controllaAudio.pause();
+	energyAudio.pause();
+	hotlineAudio.pause();
+
+	// resets chance counter
+	chances = 14;
+	// resets letters guessed 
+	lettersGuessed = [];
+	// resets letters/spaces remaining
+	lettersAndSpaces = [];
+	// resets hidden spaces in title
+	hiddenSpaces = [];
+
+	// call on a random word from the words array
+	word = words[Math.floor(Math.random()*words.length)];
+		console.log(word);
+	// if word is matching song, play designated song 
+	for(var i = 0; i < word.length; i++){
+		if(word === "energy"){
+			energyAudio.play();
+		}
+		else if(word === "controlla"){
+			controllaAudio.play();
+		}
+		else if(word === "hotline bling"){
+			hotlineAudio.play();
+		}
+	};
+
+
+	// gets all letters in random word
+	lettersInWord = word.split("");
 
 
 
-// 		var wins;  // stored wins
-// 		var chances; // chances
-// 		var spaces; // number of spaces "-"
-// 		var guesses; // stored letters guessed
+	// counts letters in word
+	spaces = lettersInWord.length;
 
-		// var showWins = document.getElementbyId("myWins");
-		// var showChances = document.getElementbyId("myChances");
-		// var showGuesses = document.getElementbyId("myGuesses");
+	for(var i = 0; i < spaces; i++){
+		lettersAndSpaces.push("_");
+	} if(i === " "){
+		hiddenSpaces.push(" ");
+	}
 
-		var word = words[Math.floor(Math.random()*words.length)];
-			console.log(word);
+	console.log(lettersAndSpaces);
 
-		var answerArray = [];
-			for (var i = 0; i < word.length; i++){
-				console.log(answerArray[i] = "_" );
+	// update HTML to show new counts
+	document.getElementById("chances").innerHTML = chances;
+	document.getElementById("spaces").innerHTML = hiddenSpaces.join(" ");
+	document.getElementById("spaces").innerHTML = lettersAndSpaces.join(" ");
+	document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
+};
 
+// function to find if user's input matches letters in srandom word
+function checkLetters(letter) {
+	// boolean; is letter in word?
+	var letterFound = false;
+	// if letter is in word, change boolean to true
+	for(var i = 0; i < spaces; i++){
+		if(word[i] === letter){
+			letterFound = true;
+		}
+	}
+	// if true...
+	if(letterFound){
+		for(var i = 0; i < spaces; i++){
+			if(word[i] === letter){
+				// replace space with letter
+				lettersAndSpaces[i] = letter;
 			}
-		var remainingLetters = word.length;
+		}
+	}
+	// if false...add guessed letter to lettersGuessed array and subtract from total chances
+	else{
+		lettersGuessed.push(letter);
+		chances--;
+	}
 
-		document.onkeyup = function(){
-			var userguess = String.fromCharCode(event.keyCode).toLowerCase();
-			alert("Guess");
-
-		// var html = "<p>Press any key to start!</p>" + 
-		// 	"<p>wins: " + wins + "</p>" +
-		// 	"<p>chances left: " + chances left + "</p>" +
-		// 	"<p>letters guessed: " + letters guessed + "</p>" ;
-
+};
 
 
+function roundComplete() {
+	// update HTML to show new counts
+	document.getElementById("chances").innerHTML = chances;
+	document.getElementById("spaces").innerHTML = lettersAndSpaces.join(" ");
+	document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
 
-}
+	// if user matches all letters, add to win score, restart game
+	if(lettersInWord.toString() === lettersAndSpaces.toString()){
+		wins++;
+		// update HTML
+		document.getElementById("wins").innerHTML = wins;
+		startGame();
+	}
+	// if user runs out of chances, update loss score and restart game
+	else if(chances === 0){
+		losses++;
+		// update HTML
+		document.getElementById("losses").innerHTML = losses;
+		startGame();
+	}
+};
 
+
+startGame();
+
+
+document.onkeyup = function(){
+	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	checkLetters(userguess);
+	roundComplete();
+};
 
